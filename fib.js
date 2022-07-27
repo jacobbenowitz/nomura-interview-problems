81ae84385eeb513cc0f645adc21d581fa179d520
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-07-27 12:52:47 -0400

    solved fib problem, return array

--- fib.js
+++ fib.js
@@ -10,9 +10,15 @@
 // Answer:         1 + 1 = 2  =>  [1, 1, 2] 
 
 const fib = (n) => {
-  if (n === 1) return 1; // basecase 1
-  if (n === 2) return 1; // basecase 2
-  return fib(n - 1) + fib(n - 2);
+  if (n === 1) return [1]; // basecase 1
+  if (n === 2) return [1, 1]; // basecase 2
+  const arr = fib(n - 1);     // create initial array
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  // recursive step pushing return value of the sum of prev to numbers in the sequence
+  return arr; // return array after the entire call stack evaluates
 }
 
-console.log(10)
+console.log(fib(1))   // => expected: [1]
+console.log(fib(2))   // => expected: [1, 1]
+console.log(fib(3))   // => expected: [1, 1, 2]
+console.log(fib(10))  // => expected: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
